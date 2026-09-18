e5d31feda337d5918287c766e360a18a4434125b
xthan <hackattendant@yahoo.com>
2026-09-18 10:38:13 -0400

    update gotta delete and rejnstall each time

--- ACSProject/PropHunt/app/src/main/kotlin/com/xthan/prophunt/MainActivity.kt
+++ ACSProject/PropHunt/app/src/main/kotlin/com/xthan/prophunt/MainActivity.kt
@@ -2,30 +2,36 @@ package com.xthan.prophunt
 
 import com.xthan.prophunt.R
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        
+        // Hide system bars and enable immersive sticky mode
+        val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        // Optional: Remove window padding listener if you want content behind bars, 
+        // or keep it if your HTML game layout handles its own safe areas.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-
 
         val webV: WebView = findViewById(R.id.myWebView)
         webV.settings.javaScriptEnabled = true
         webV.loadUrl("file:///android_asset/index.html")
-
     }
-
-
 }
